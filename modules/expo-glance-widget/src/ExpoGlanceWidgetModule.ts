1ded7a8e5a27c6560a4e7bf4e4fd5f7c4c681cb4
tigawanna <denniskinuthiawaweru@gmail.com>
2025-07-02 16:06:38 +0300

    fixes

--- modules/expo-glance-widget/src/ExpoGlanceWidgetModule.ts
+++ modules/expo-glance-widget/src/ExpoGlanceWidgetModule.ts
@@ -1,12 +1,14 @@
 import { NativeModule, requireNativeModule } from "expo";
-
-import { ExpoGlanceWidgetModuleEvents } from "./ExpoGlanceWidgetModule.types";
+import { ExpoGlanceWidgetModuleEvents, WidgetCreationResult } from "./ExpoGlanceWidgetModule.types";
 
 declare class ExpoGlanceWidgetModule extends NativeModule<ExpoGlanceWidgetModuleEvents> {
+  createWidget(): Promise<WidgetCreationResult>;
   PI: number;
   hello(): string;
   setValueAsync(value: string): Promise<void>;
+
+  // Add this:
+  updateWakatimeHours(hours: string): Promise<void>;
 }
 
-// This call loads the native module object from the JSI.
 export default requireNativeModule<ExpoGlanceWidgetModule>("ExpoGlanceWidgetModule");

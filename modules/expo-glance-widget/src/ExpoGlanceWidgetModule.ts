a985cd09841cfe2607f69563b0e012a0fea44246
tigawanna <denniskinuthiawaweru@gmail.com>
2025-07-02 13:52:45 +0300

    fix: start expo modulefrom basics

--- modules/expo-glance-widget/src/ExpoGlanceWidgetModule.ts
+++ modules/expo-glance-widget/src/ExpoGlanceWidgetModule.ts
@@ -1,19 +1,12 @@
-import { NativeModule, requireNativeModule } from 'expo';
+import { NativeModule, requireNativeModule } from "expo";
 
-import { ExpoGlanceWidgetModuleEvents, SharedPreferencesOptions, SharedPreferencesValue } from './ExpoGlanceWidget.types';
+import { ExpoGlanceWidgetModuleEvents } from "./ExpoGlanceWidgetModule.types";
 
 declare class ExpoGlanceWidgetModule extends NativeModule<ExpoGlanceWidgetModuleEvents> {
   PI: number;
   hello(): string;
   setValueAsync(value: string): Promise<void>;
-  
-  // Shared Preferences methods
-  setSharedPreferenceAsync(key: string, value: SharedPreferencesValue, options?: SharedPreferencesOptions): Promise<void>;
-  getSharedPreferenceAsync(key: string, options?: SharedPreferencesOptions): Promise<SharedPreferencesValue>;
-  removeSharedPreferenceAsync(key: string, options?: SharedPreferencesOptions): Promise<void>;
-  clearSharedPreferencesAsync(options?: SharedPreferencesOptions): Promise<void>;
-  getAllSharedPreferencesAsync(options?: SharedPreferencesOptions): Promise<Record<string, SharedPreferencesValue>>;
 }
 
 // This call loads the native module object from the JSI.
-export default requireNativeModule<ExpoGlanceWidgetModule>('ExpoGlanceWidget');
+export default requireNativeModule<ExpoGlanceWidgetModule>("ExpoGlanceWidgetModule");

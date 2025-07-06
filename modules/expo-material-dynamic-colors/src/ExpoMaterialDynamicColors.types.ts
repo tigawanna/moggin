31ccb6052c34902566772cf4220cb30575d5f9bc
tigawanna <denniskinuthiawaweru@gmail.com>
2025-07-06 15:32:49 +0300

    feat: add a material 3 dynamic colors module

--- modules/expo-material-dynamic-colors/src/ExpoMaterialDynamicColors.types.ts
+++ modules/expo-material-dynamic-colors/src/ExpoMaterialDynamicColors.types.ts
@@ -1,19 +1,75 @@
 import type { StyleProp, ViewStyle } from 'react-native';
 
-export type OnLoadEventPayload = {
-  url: string;
+enum ElevationLevels {
+  "level0",
+  "level1",
+  "level2",
+  "level3",
+  "level4",
+  "level5",
+}
+
+export type SystemScheme = {
+  primary: string;
+  onPrimary: string;
+  primaryContainer: string;
+  onPrimaryContainer: string;
+
+  secondary: string;
+  onSecondary: string;
+  secondaryContainer: string;
+  onSecondaryContainer: string;
+
+  tertiary: string;
+  onTertiary: string;
+  tertiaryContainer: string;
+  onTertiaryContainer: string;
+
+  background: string;
+  onBackground: string;
+
+  surface: string;
+  onSurface: string;
+  surfaceVariant: string;
+  onSurfaceVariant: string;
+
+  outline: string;
+  outlineVariant: string;
+
+  inverseSurface: string;
+  inverseOnSurface: string;
+  inversePrimary: string;
 };
 
-export type ExpoMaterialDynamicColorsModuleEvents = {
-  onChange: (params: ChangeEventPayload) => void;
+export type MaterialDynamicColors = SystemScheme & {
+  error: string;
+  onError: string;
+  errorContainer: string;
+  onErrorContainer: string;
+
+  shadow: string;
+  scrim: string;
+  surfaceDisabled: string;
+  onSurfaceDisabled: string;
+  backdrop: string;
+
+  surfaceContainer: string;
+  surfaceContainerLow: string;
+  surfaceContainerLowest: string;
+  surfaceContainerHigh: string;
+  surfaceContainerHighest: string;
+  surfaceBright: string;
+  surfaceDim: string;
+  surfaceTint: string;
+
+  elevation: {
+    [key in keyof typeof ElevationLevels]: string;
+  };
 };
 
-export type ChangeEventPayload = {
-  value: string;
+export type MaterialDynamicTheme = {
+  light: MaterialDynamicColors;
+  dark: MaterialDynamicColors;
 };
 
-export type ExpoMaterialDynamicColorsViewProps = {
-  url: string;
-  onLoad: (event: { nativeEvent: OnLoadEventPayload }) => void;
-  style?: StyleProp<ViewStyle>;
-};
+

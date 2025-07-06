9109ab453a4a0f79f7de258d157caf150450bd63
tigawanna <denniskinuthiawaweru@gmail.com>
2025-07-06 23:44:49 +0300

    Refactor code structure for improved readability and maintainability

---
+++ lib/datastore/store.ts
@@ -0,0 +1,20 @@
+import DatastoreModel from "@/modules/expo-glance-widget"
+
+
+export const datastoreKeys = {
+    wakatimeApiKey: "wakatime_api_key",
+    wakatimeTotalTime: "wakatime_total_time",
+    wakatimeCurrentProject: "wakatime_current_project",
+    wakatimeLastSync: "wakatime_last_sync",
+} as const
+export function updateWakatimeWidgetKey(key: string | null) {
+  if(!key) {
+    return DatastoreModel.deleteDatastoreValue(datastoreKeys.wakatimeApiKey);
+  }
+  return DatastoreModel.updateDatastoreValue(datastoreKeys.wakatimeApiKey, key);
+}
+
+
+export function getWakatimeWidgetKey() {
+  return DatastoreModel.getDatastoreValue(datastoreKeys.wakatimeApiKey);
+}

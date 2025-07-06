7e9a882762cf1ddc4848dc52a1e4783654380720
tigawanna <denniskinuthiawaweru@gmail.com>
2025-07-06 15:57:08 +0300

    add: material3 native module

--- modules/expo-material-dynamic-colors/index.ts
+++ modules/expo-material-dynamic-colors/index.ts
@@ -1,5 +1,7 @@
 // Reexport the native module. On web, it will be resolved to ExpoMaterialDynamicColorsModule.web.ts
 // and on native platforms to ExpoMaterialDynamicColorsModule.ts
+export * from './src/ExpoMaterialDynamicColors.types';
 export { default } from './src/ExpoMaterialDynamicColorsModule';
-export { default as ExpoMaterialDynamicColorsView } from './src/ExpoMaterialDynamicColorsView';
-export * from  './src/ExpoMaterialDynamicColors.types';
+
+
+

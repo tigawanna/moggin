93e1c406fcb3fd8571fde1dc7a2ecafa299fc15f
tigawanna <denniskinuthiawaweru@gmail.com>
2025-07-12 19:40:32 +0300

    Implement code changes to enhance functionality and improve performance

--- app.config.ts
+++ app.config.ts
@@ -52,6 +52,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     },
     plugins: [
       "expo-router",
+      "expo-background-task",
       [
         "expo-splash-screen",
         {

b85f5f3dd8bb085e469e467c7800fe6587acb3bf
tigawanna <denniskinuthiawaweru@gmail.com>
2025-07-14 16:37:34 +0300

    fix: lates changes with work manger

--- utils/date.ts
+++ utils/date.ts
@@ -72,7 +72,7 @@ export function dateToDayHoursMinutesSeconds(date: Date): string {
   const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
+  // const seconds = String(date.getSeconds()).padStart(2, '0');
 
-  return `${day}/${month} ${hours}:${minutes}:${seconds}`;
+  return `${day}/${month} ${hours}:${minutes}`;
 }
